34dd05633bc68f8dfdeb7e3d90e79443873829d5
hiroshi-ota <kamil.kazmierowski@gmail.com>
2015-10-24 18:04:53 +0200

    v 0.1: Start
    A jQuery extension to animate letters

---
+++ js/jLetters.js
@@ -0,0 +1,35 @@
+(function(jQuery){
+
+    $.fn.jLetters = function() {
+        var args = {},
+            text;
+        /*
+            - initialise jLetters
+            - set args & text
+            @args: list of arguments passed by user
+            @text: optional text (string) passed by user
+        */
+        (function(arguments){
+            if(typeof arguments[0] !== 'object'){
+                text = arguments[0];
+                arguments = Array.prototype.slice.call(arguments, 1);
+            }
+            args = arguments[0];
+        }(arguments))
+
+        function Animate(elem){
+            var string = elem.text();
+            $('body').append(args.before + string + args.after);
+        }
+        Animate($(this));
+
+    }
+
+    //Exception control:
+    function ExceptionLog(message){
+        this.name = 'jLetters Error';
+        this.message = message;
+    }
+    ExceptionLog.prototype = Error.prototype;
+
+}())

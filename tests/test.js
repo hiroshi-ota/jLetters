a59e8ea8f2d52b2631731861bf45beabe7c14bdd
hiroshi-ota <kamil.kazmierowski@gmail.com>
2015-11-01 02:10:52 +0100

    v. 0.1.6: New build (more obiect oriented approach)
    + simple animation

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,14 @@
-module('Configuration');
+module('Configuration', {
+    setup: function() {
+        someText = 'some text';
+        div = document.createElement('div');
+        div.class = 'myClass';
+        div.innerHTML = 'some text in div';
+    }
+});
 
 test('initialisation', function () {
     equal(typeof $.fn.jLetters, 'function', 'jLetters is a part of jQuery');
-    equal(typeof $(this).jLetters().data('jLetters'), 'object', 'api is there');
+    equal(typeof $(div).jLetters().data('jLetters'), 'object', 'api is there');
+    $(div).jLetters();
 })
-
-module('jLetters: Errors');
